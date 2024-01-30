23b3c263b3a655bec7874830fcf706c5d44ac5d5
Flavien Chenu <flavienchenu@hotmail.com>
2024-01-30 12:57:13 +0100

    Added lib in demo1

---
+++ demos/demo-1/lib/my/my.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** tek-workshop-makefiles
+** File description:
+** print
+*/
+
+#pragma once
+
+void my_print_plus(const char *str);
+void my_print_stars(const char *str);
+void my_print_minus(const char *str);
+void my_print_dots(const char *str);
---
+++ demos/demo-1/lib/my/my_print_stars.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** tek-workshop-makefiles
+** File description:
+** my_print_stars
+*/
+
+#include <stdio.h>
+
+void my_print_stars(const char *str)
+{
+    printf("**** [%s] ****\n", str);
+}
